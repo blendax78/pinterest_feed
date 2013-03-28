d32496d452d0d413c78d0b3ee54657d6953f5601
blendax78 <eric@vintimilla.org>
2013-03-27 21:48:54 -0400

    Removed extra space. Made speed improvements.

--- popup.js
+++ popup.js
@@ -1,13 +1,14 @@
 
-var kittenGenerator = {
+var Loader = {
 
-  requestKittens: function() {
-
+  run: function() {
+	  var w = $('#pinterestIframe'); 
+	  w.attr('src', "http://m.pinterest.com");
   },
 
 };
 
 // Run our kitten generation script as soon as the document's DOM is ready.
 document.addEventListener('DOMContentLoaded', function () {
-  //kittenGenerator.requestKittens();
+  Loader.run();
 });
